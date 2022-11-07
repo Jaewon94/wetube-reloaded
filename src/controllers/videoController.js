6e8aa14418838c9b59670636a9145ffd900809fe
Jaewon94 <104521276+Jaewon94@users.noreply.github.com>
2022-11-07 16:53:27 +0900

    update

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,34 @@
-export const trending = (req, res) => res.send("Home Page Videos.");
-export const see = (req, res) => {
-
-  return res.send(`Watch Video #${req.params.id}`);
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "First Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 min ago",
+      views: 19,
+      id: 1,
+    },
+    {
+      title: "Second Video",
+      rating: 3,
+      comments: 1,
+      createdAt: "1 min ago",
+      views: 19,
+      id: 2,
+    },
+    {
+      title: "Third Video",
+      rating: 4,
+      comments: 4,
+      createdAt: "2 min ago",
+      views: 199,
+      id: 3,
+    },
+  ];
+  return res.render("home", {pageTitle : "home", videos});
 }
-export const edit = (req, res) => {
-  console.log(req.params);
-  return res.send(`Edit Video  #${req.params.id}`);
-}
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
 export const remove = (req, res) => {
   console.log(req.params);
   return res.send(`Remove Video  #${req.params.id}`);
@@ -14,3 +36,4 @@ export const remove = (req, res) => {
 export const upload = (req, res) => res.send("Upload Video");
 
 export const search = (req, res) => res.send("Search Video");
+
